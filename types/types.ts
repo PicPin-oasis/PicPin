3ad31c09e5874ffdfc37c7284050406a3203a5ee
energy1-914 <dlawldnjs5@daum.net>
2023-11-06 04:41:56 +0900

    Feat: 메인 페이지 초기 구현 / 타입 파일 추가

---
+++ types/types.ts
@@ -0,0 +1,12 @@
+export interface ButtonProps {
+  text: string;
+  onClick: () => void;
+}
+export interface LoginFormValue {
+  email: string;
+  password: string;
+}
+export interface User {
+  email: string;
+  password: string;
+}
