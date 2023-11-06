7ec1717fdec3a5e0587b805409981ed392759d9d
energy1-914 <dlawldnjs5@daum.net>
2023-11-07 07:11:45 +0900

    Fix: 페이지명 변경

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,9 +1,9 @@
 import Header from "@/components/header/Header";
 import KakaoMap from "./(componenets)/KaKaoMap";
 
-export default function MainPage() {
+export default function MapPage() {
   return (
-    <div className="w-screen h-screen  flex flex-col items-center">
+    <div className="w-screen h-screen flex flex-col items-center">
       <Header />
       <KakaoMap />
     </div>
