101fa1adaabe72b81b669a7725c18a0eb8856409
energy1-914 <dlawldnjs5@daum.net>
2023-11-09 02:53:53 +0900

    Fix: api 경로 및 이미지 속도 이슈로 config 파일 수정

--- next.config.js
+++ next.config.js
@@ -3,12 +3,16 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        // OAuth 인증 코드를 카카오 서버로 전달하기 위한 프록시 설정
         source: "/oauth2/code/kakao",
         destination: "https://api.picpin.life/oauth2/code/kakao",
       },
+      {
+        source: "/accounts/my-profile",
+        destination: "https://api.picpin.life/accounts/my-profile",
+      },
     ];
   },
+  output: 'standalone',
   trailingSlash: true,
 };
 
