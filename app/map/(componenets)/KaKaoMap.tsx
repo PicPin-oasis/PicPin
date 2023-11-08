1f1c85db653a30a91b93fb5c663c849923929d3a
energy1-914 <dlawldnjs5@daum.net>
2023-11-08 16:20:51 +0900

    Fix: 지도 로드 수정

--- app/map/(componenets)/KaKaoMap.tsx
+++ app/map/(componenets)/KaKaoMap.tsx
@@ -8,21 +8,40 @@ const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT
 
 const KakaoMap = () => {
   const [location, setLocation] = useState({ lat: 33.450701, lng: 126.570667 });
-
+  const [mapLoaded, setMapLoaded] = useState(false);
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function (position) {
-        const lat = position.coords.latitude; // 위도
-        const lng = position.coords.longitude; // 경도
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
         setLocation({ lat, lng });
       });
     }
   }, []);
 
+  useEffect(() => {
+    if (window.kakao && window.kakao.maps) {
+      window.kakao.maps.load(() => {
+        setMapLoaded(true);
+      });
+    }
+  }, []);
   return (
     <div className="w-4/6 h-5/6 py-7">
-      <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
-      <Map center={location} style={{ width: "100%", height: "90%" }}></Map>
+      <Script
+        src={KAKAO_SDK_URL}
+        strategy="afterInteractive"
+        onLoad={() => {
+          window.kakao &&
+            window.kakao.maps &&
+            window.kakao.maps.load(() => {
+              setMapLoaded(true);
+            });
+        }}
+      />
+      {mapLoaded && (
+        <Map center={location} style={{ width: "100%", height: "90%" }}></Map>
+      )}
     </div>
   );
 };
