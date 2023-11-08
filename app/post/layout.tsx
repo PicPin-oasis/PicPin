1a2f048fdddeb4488e22d17f8989df07093558fa
energy1-914 <dlawldnjs5@daum.net>
2023-11-09 05:51:13 +0900

    Feat: 포스트 페이지 초기 레이아웃

---
+++ app/post/layout.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Header from "@/components/header/Header";
+
+const PostPageLayout = () => {
+  return (
+    <div className="w-full flex flex-col  justify-center  items-center">
+      <Header />
+    </div>
+  );
+};
+export default PostPageLayout;
---
+++ app/post/page.tsx
@@ -0,0 +1,5 @@
+import PostPageLayout from "./layout";
+
+export default function PostPage() {
+  return <PostPageLayout />;
+}
