25f132d6c152179dcf7c9d48e895c8edc3c53ef4
energy1-914 <dlawldnjs5@daum.net>
2023-11-28 00:33:44 +0900

    Fix: Provider 수정

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,24 +1,21 @@
 "use client";
+
 import { store, persistor } from "@/redux/store";
 import { Provider as ReduxProvider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { PersistGate } from "redux-persist/integration/react";
 import { ChildrenProps } from "@/types/types";
-import { Header } from "@/components/common/Header";
-import { Navbar } from "@/components/common/Navbar";
+import { useState } from "react";
 
 const Provider = ({ children }: ChildrenProps) => {
-  const queryClient = new QueryClient();
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
       <ReduxProvider store={store}>
-        <PersistGate persistor={persistor}>
-          <Header />
-          {children}
-          <Navbar />
-        </PersistGate>
+        <PersistGate persistor={persistor}>{children}</PersistGate>
       </ReduxProvider>
     </QueryClientProvider>
   );
