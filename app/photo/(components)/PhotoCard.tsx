e17c0f925b03a82eaf1613c079fd1250d2dab591
energy1-914 <dlawldnjs5@daum.net>
2023-12-06 19:35:14 +0900

    Style: 레이아웃 / 버튼 컴포넌트 스타일 변경

--- app/photo/(components)/PhotoCard.tsx
+++ app/photo/(components)/PhotoCard.tsx
@@ -7,14 +7,16 @@ interface Props {
 }
 export const PhotoCard = ({ src }: Props) => {
   return (
-    <Image
-      // src={`https://image-store.picpin.life/${src}`}
-      src="https://image-store.picpin.life/images/KakaoTalk_20220201_105518089_01.jpg"
-      alt={src}
-      width={85}
-      height={85}
-      className="rounded-lg"
-      priority
-    />
+    <div onClick={() => {}}>
+      <Image
+        // src={`https://image-store.picpin.life/${src}`}
+        src="https://image-store.picpin.life/images/KakaoTalk_20220201_105518089_01.jpg"
+        alt={src}
+        width={85}
+        height={85}
+        className="rounded-lg"
+        priority
+      />
+    </div>
   );
 };
