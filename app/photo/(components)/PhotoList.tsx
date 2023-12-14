71b924594c0514543b9abb777c4f201c51b6ea06
energy1-914 <dlawldnjs5@daum.net>
2023-12-14 21:20:12 +0900

    Fix: 등록된 사진 없을 시 로직 변경

--- app/photo/(components)/PhotoList.tsx
+++ app/photo/(components)/PhotoList.tsx
@@ -13,29 +13,25 @@ export const PhotoList = () => {
   );
   if (isLoading) return <Loading />;
   if (isError) return <Error text="잘못된 접근입니다. 다시 시도해주세요." />;
-
+  if (!data?.length) return <div>등록된 사진이 없습니다.</div>;
   return (
     <div className="grow w-full">
-      {sortedArr?.length ? (
-        <div className="flex flex-col gap-7 mt-5">
-          {sortedArr.map((item) => (
-            <div key={item.taken_photo_date}>
-              <Text text={item.taken_photo_date} />
-              <div className="w-full grid grid-cols-4 place-items-center">
-                {item.photo_cards.map((photo) => (
-                  <PhotoCard
-                    src={photo.expose_image_url}
-                    key={photo.id}
-                    id={photo.id.toString()}
-                  />
-                ))}
-              </div>
+      <div className="flex flex-col gap-7 mt-5">
+        {sortedArr?.map((item) => (
+          <div key={item.taken_photo_date}>
+            <Text text={item.taken_photo_date} />
+            <div className="w-full grid grid-cols-4 place-items-center">
+              {item.photo_cards.map((photo) => (
+                <PhotoCard
+                  src={photo.expose_image_url}
+                  key={photo.id}
+                  id={photo.id.toString()}
+                />
+              ))}
             </div>
-          ))}
-        </div>
-      ) : (
-        <div>사진이 없습니다 !</div>
-      )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
