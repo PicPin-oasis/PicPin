2ea9659754e9c8a33d3e4aef09b28c6efcfe0d3c
energy1-914 <dlawldnjs5@daum.net>
2023-12-14 20:35:51 +0900

    Fix: 에러, 로딩 컴포넌트 사용

--- app/photo/(components)/AlbumListSelectBox.tsx
+++ app/photo/(components)/AlbumListSelectBox.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useGetAlbums } from "@/apis/axios/album/getAlbums";
+import { Error } from "@/components/common/Error";
+import { Loading } from "@/components/common/Loading";
 import { SelectBox } from "@/components/common/SelectBox";
 import { useAppSelector } from "@/redux/store";
 import { AlbumProps, PhotoUploaderProps } from "@/types/types";
@@ -14,8 +16,8 @@ export const AlbumListSelectBox = ({ setSubmitInfo }: Props) => {
   const { accessToken } = useAppSelector((state) => state.accessToken);
   const { data, isLoading, isError } = useGetAlbums();
 
-  if (isLoading) return <div>로딩 중... </div>;
-  if (isError) return <div>잘못된 접근입니다 !</div>;
+  if (isLoading) return <Loading />;
+  if (isError) return <Error text="잘못된 접근입니다. 다시 시도해주세요." />;
 
   return (
     <SelectBox
