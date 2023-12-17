6a087c8083aa3995df175c30652d4abc416b53b4
energy1-914 <dlawldnjs5@daum.net>
2023-12-17 23:17:54 +0900

    Feat: 포토 디테일 페이지 하단리스트 추가

---
+++ app/photo/[id]/(components)/PhotoList.tsx
@@ -0,0 +1,23 @@
+import axiosInstance from "@/apis/axios/instance";
+import { GetPhotosProps } from "@/apis/axios/photos/getPhotos";
+import Image from "next/image";
+import { PhotoCard } from "../../(components)/PhotoCard";
+
+export const PhotoList = async () => {
+  const { data } = await axiosInstance.get<{
+    photo_sections: GetPhotosProps[];
+  }>("/photo-sections");
+  return (
+    <div className="w-full flex overflow-x-auto gap-2 py-2">
+      {data.photo_sections.map((item) =>
+        item.photo_cards.map((photo) => (
+          <PhotoCard
+            src={photo.expose_image_url}
+            key={photo.id}
+            id={photo.id.toString()}
+          />
+        )),
+      )}
+    </div>
+  );
+};
--- app/photo/[id]/layout.tsx
+++ app/photo/[id]/layout.tsx
@@ -1,6 +1,7 @@
 import axiosInstance from "@/apis/axios/instance";
-import PhotoDetail from "./(components)/PhotoDetail";
+import { PhotoDetail } from "./(components)/PhotoDetail";
 import { PhotoDetailProps } from "@/apis/axios/photos/getPhotoDetail";
+import { PhotoList } from "./(components)/PhotoList";
 
 interface LayoutProps {
   params?: { id: string };
@@ -10,8 +11,9 @@ async function PhotoDetailPageLayout({ params }: LayoutProps) {
   const { id } = params!;
   const { data } = await axiosInstance.get<PhotoDetailProps>(`/photos/${id}`);
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center gap-1 OwO">
+    <div className="w-full h-full flex flex-col items-center gap-1">
       <PhotoDetail data={data} />
+      <PhotoList />
     </div>
   );
 }
