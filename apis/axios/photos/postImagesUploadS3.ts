7d07b1c8cf3b453d39ac94cf8e8ea799b7a4fc40
BeyJ1-914 <bey@o-my.us>
2024-01-01 16:39:24 +0900

    Rename: 파일명 변경

--- apis/axios/photos/postImagesUploadS3.ts
+++ apis/axios/photos/postImagesUploadS3.ts
@@ -1,12 +1,13 @@
 import { useMutation, useQueryClient } from "react-query";
 import axiosInstance from "../instance";
+import { queryKeyFactory } from "../queryKeyFactory";
 
 interface Props {
   upload_file_paths: string[];
   failed_upload_filenames: string[];
 }
 
-export const postPhotosUploadS3 = async (formData: FormData) => {
+export const postImagesUploadS3 = async (formData: FormData) => {
   const { data } = await axiosInstance.post<Props>("photos/upload", formData, {
     headers: {
       "Content-Type": "multipart/form-data",
@@ -15,11 +16,11 @@ export const postPhotosUploadS3 = async (formData: FormData) => {
   return data;
 };
 
-export const usePostPhotosUploadS3 = () => {
+export const usePostImagesUploadS3 = () => {
   const queryClient = useQueryClient();
-  return useMutation(postPhotosUploadS3, {
+  return useMutation(postImagesUploadS3, {
     onSuccess: () => {
-      queryClient.invalidateQueries(["PHOTOS"]);
+      queryClient.invalidateQueries(queryKeyFactory.GET_PHOTOS());
     },
     onError: (err) => {
       console.log("err!!", err);
