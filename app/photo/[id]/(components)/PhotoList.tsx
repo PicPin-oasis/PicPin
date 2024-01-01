dca104a166fbc8c93074090f40dcdf3ded2c4f48
BeyJ1-914 <bey@o-my.us>
2024-01-01 16:43:59 +0900

    Refactor: 에러 처리 로직 추가

--- app/photo/[id]/(components)/PhotoList.tsx
+++ app/photo/[id]/(components)/PhotoList.tsx
@@ -1,22 +1,28 @@
 "use client";
 
+import Loading from "@/components/common/Loading";
+import Error from "@/components/common/Error";
 import PhotoCard from "../../(components)/PhotoCard";
 import { useGetPhotos } from "@/apis/axios/photos/getPhotos";
 
 export const PhotoList = () => {
   const { data, isLoading, isError } = useGetPhotos();
+
+  if (isLoading) return <Loading />;
+  if (isError || !data)
+    return <Error text="잘못된 접근입니다. 다시 시도해주세요." />;
+
   return (
     <div className="w-full flex overflow-x-auto gap-2 py-2">
-      {data &&
-        data.map((item) =>
-          item.photo_cards.map((photo) => (
-            <PhotoCard
-              src={photo.expose_image_url}
-              key={photo.id}
-              id={photo.id.toString()}
-            />
-          )),
-        )}
+      {data.map((item) =>
+        item.photo_cards.map((photo) => (
+          <PhotoCard
+            src={photo.expose_image_url}
+            key={photo.id}
+            id={photo.id.toString()}
+          />
+        )),
+      )}
     </div>
   );
 };
