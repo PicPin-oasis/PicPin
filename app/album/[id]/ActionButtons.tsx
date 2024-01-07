21480c870b36a3fdbdd134924e831db177f40f0e
energy1-914 <dlawldnjs5@daum.net>
2024-01-08 01:58:50 +0900

    Feat: 앨범 상세 목록 구현

--- app/album/[id]/ActionButtons.tsx
+++ app/album/[id]/ActionButtons.tsx
@@ -1,9 +1,38 @@
 "use client";
 
+import { useDeleteAlbumMutation } from "@/apis/axios/album/deleteAlbum";
+import Toast from "@/components/common/Toast";
 import WhiteButton from "@/components/common/WhiteButton";
+import { setEditStatus } from "@/redux/editStatusSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/store";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-interface Props {}
-const ActionButtons = ({}: Props) => {
+interface Props {
+  albumId: number;
+}
+const ActionButtons = ({ albumId }: Props) => {
+  const [toast, setToast] = useState(false);
+  const route = useRouter();
+  const dispatch = useAppDispatch();
+  const {
+    mutate: deleteMutation,
+    isSuccess: isDeleteSuccess,
+    isError: isDeleteError,
+  } = useDeleteAlbumMutation({
+    onSuccess: () => {
+      setToast(true);
+      setTimeout(() => {
+        route.push("/album");
+      }, 2000);
+    },
+  });
+  const handleUpdateAlbum = () => {
+    dispatch(setEditStatus({ isEditing: true, type: "album" }));
+  };
+  const handleDeleteAlbum = () => {
+    deleteMutation(albumId);
+  };
   return (
     <div className="flex gap-1">
       <WhiteButton
@@ -13,14 +42,15 @@ const ActionButtons = ({}: Props) => {
       />
       <WhiteButton
         text="수정"
-        onClick={() => {}}
+        onClick={handleUpdateAlbum}
         classNames="w-fit px-2 text-xs rounded-full h-fit"
       />
       <WhiteButton
         text="삭제"
-        onClick={() => {}}
+        onClick={handleDeleteAlbum}
         classNames="w-fit px-2 text-xs rounded-full h-fit"
       />
+      {toast && <Toast text="삭제되었습니다." />}
     </div>
   );
 };
